09bd641c7c99e0724a3201b5f320852cb6b5b5ff
GayanM <gayan@Gayans-MacBook-Pro.local>
2025-04-19 07:10:29 +0800

    Fixing script

--- scripts/export_google_workspace_app.ps1
+++ scripts/export_google_workspace_app.ps1
@@ -1,3 +1,11 @@
+$tenantId = $env:AZURE_TENANT_ID
+$clientId = $env:AZURE_CLIENT_ID
+$clientSecret = $env:AZURE_CLIENT_SECRET
+
+$secureSecret = ConvertTo-SecureString $clientSecret -AsPlainText -Force
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $secureSecret
+
+Connect-MgGraph -TenantId $tenantId -ClientSecretCredential $creds
 
 # Connect to Microsoft Graph
 Connect-MgGraph -Scopes "Application.Read.All", "Directory.Read.All", "AppRoleAssignment.Read.All"
