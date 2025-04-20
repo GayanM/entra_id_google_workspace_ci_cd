a061115368cef5451a224ae4de2835cf6d7b030f
GayanM <gayan@Gayans-MacBook-Pro.local>
2025-04-21 05:43:19 +0800

    Fixing script

--- scripts/import_google_workspace_app.ps1
+++ scripts/import_google_workspace_app.ps1
@@ -2,10 +2,11 @@
 # Connect to Microsoft Graph
 Connect-MgGraph -Scopes "Application.ReadWrite.All", "Directory.ReadWrite.All"
 
+$importPath = Join-Path $PSScriptRoot "../config/google_workspace_config.json"
 # Load configuration
-$config = Get-Content "../config/google_workspace_config.json" | ConvertFrom-Json
+$config = Get-Content $importPath | ConvertFrom-Json
 
 # Note: For gallery apps like Google Workspace, manual app creation is required
 # Here we fetch the existing app instead
-$sp = Get-MgServicePrincipal -Filter "DisplayName eq 'Google Workspace'"
+$sp = Get-MgServicePrincipal -Filter "DisplayName eq 'Google Cloud / G Suite Connector by Microsoft'"
 Write-Output "App exists with ID: $($sp.Id)"
